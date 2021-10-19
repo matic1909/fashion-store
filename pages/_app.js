234df278515977aefe3564aae9bbe44a762b55de
Nils Matic <matic1909@gmail.com>
2021-10-19 15:20:50 +0200

    added progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import Page from '../components/Page';
-import '../styles/globals.css';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
