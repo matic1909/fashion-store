1797cb9f3e0184195a7200dd598c034c8a8407ee
Nils Matic <matic1909@gmail.com>
2021-10-21 17:08:38 +0200

    query current user

--- components/Nav.js
+++ components/Nav.js
@@ -1,21 +1,28 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import { useUser } from './User';
 
 export default function Nav() {
+  const user = useUser();
   return (
     <NavStyles>
       <Link href="/products">
         <a>Products</a>
       </Link>
-      <Link href="/sell">
-        <a>Sell</a>
-      </Link>
-      <Link href="/orders">
-        <a>Orders</a>
-      </Link>
-      <Link href="/account">
-        <a>Account</a>
-      </Link>
+      {user && (
+        <>
+          <Link href="/sell">
+            <a>Sell</a>
+          </Link>
+          <Link href="/orders">
+            <a>Orders</a>
+          </Link>
+          <Link href="/account">
+            <a>Account</a>
+          </Link>
+        </>
+      )}
+      {!user && <Link href="/signin">Sign In</Link>}
     </NavStyles>
   );
 }
