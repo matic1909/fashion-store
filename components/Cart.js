da7c48bbf4866a836027f222f8b0a40dc1c941b0
Nils Matic <matic1909@gmail.com>
2021-10-23 21:17:17 +0200

    added context for managing cart open state

--- components/Cart.js
+++ components/Cart.js
@@ -4,6 +4,8 @@ import Supreme from './styles/Supreme';
 import { useUser } from './User';
 import formatMoney from '../lib/formatMoney';
 import calcTotalPrice from '../lib/calcTotalPrice';
+import { useCart } from '../lib/cartState';
+import CloseButton from './styles/CloseButton';
 
 const CartItemStyles = styled.li`
   padding: 1rem 0;
@@ -44,13 +46,15 @@ function CartItem({ cartItem }) {
 
 export default function Cart() {
   const me = useUser();
-  console.log(me);
+  const { cartOpen, closeCart } = useCart();
   if (!me) return null;
   return (
-    <CartStyles open>
+    <CartStyles open={cartOpen}>
       <header>
-        <Supreme>{me.name}'s Cart</Supreme>
+        <Supreme>{me.name}'s Cart</Supreme>{' '}
+        <CloseButton onClick={closeCart}>&times;</CloseButton>
       </header>
+
       <ul>
         {me.cart.map((cartItem) => (
           <CartItem key={cartItem.id} cartItem={cartItem} />
--- components/Nav.js
+++ components/Nav.js
@@ -1,10 +1,12 @@
 import Link from 'next/link';
+import { useCart } from '../lib/cartState';
 import SignOut from './SignOut';
 import NavStyles from './styles/NavStyles';
 import { useUser } from './User';
 
 export default function Nav() {
   const user = useUser();
+  const { openCart } = useCart();
   return (
     <NavStyles>
       <Link href="/products">
@@ -22,6 +24,9 @@ export default function Nav() {
             <a>Account</a>
           </Link>
           <SignOut />
+          <button type="button" onClick={openCart}>
+            My Cart
+          </button>
         </>
       )}
       {!user && <Link href="/signin">Sign In</Link>}
